579cda370f504b6595435ea57c2144240d6fd2a2
mbilal1 <bilal_taurus@live.com>
2017-11-10 13:00:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 var express = require ('express');
 var path = require ('path');
 //var mysql = require('mysql'); 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
 
 var app = express();
 
